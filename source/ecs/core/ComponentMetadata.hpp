b043c0336121d1e51a42977c657fc114177b4ee5
Mikhail Likhachev <likhachev.mischa@gmail.com>
2025-07-18 16:42:41 +0300

    refactor: metadata upgrade for reducing branching

--- source/ecs/core/ComponentMetadata.hpp
+++ source/ecs/core/ComponentMetadata.hpp
@@ -1,39 +1,37 @@
 #pragma once
-#include <typeindex>
-
 #include "Base/Platform.hpp"
 
 namespace spite
 {
+	// Forward-declare for the function pointer signature
+	class DestructionContext; 
+
 	using ComponentID = u32;
 	constexpr ComponentID INVALID_COMPONENT_ID = 0;
 
 	struct ComponentMetadata
 	{
-		// The user data pointer allows passing context (like the SharedComponentManager) to the destructor.
-		using DestructorFn = void (*)(void* componentPtr, void* userData);
+		// A single function pointer to handle all destruction side-effects.
+		using DestructionPolicyFn = void (*)(void* componentPtr, const DestructionContext& context);
 		using MoveAndDestroyFn = void (*)(void* destPtr, void* srcPtr);
 
 		ComponentID id = INVALID_COMPONENT_ID;
-		std::type_index type = typeid(nullptr_t);
 		sizet size = 0;
 		sizet alignment = 0;
-		bool isTriviallyRelocatable = true;
 		
-		DestructorFn destructor = nullptr;
-		void* destructorUserData = nullptr; // User data for the destructor
-
+		DestructionPolicyFn destructionPolicy = nullptr;
 		MoveAndDestroyFn moveAndDestroy = nullptr;
 
-		ComponentMetadata() = default;
+		constexpr ComponentMetadata() = default;
 
-		ComponentMetadata(ComponentID id,
-						  std::type_index type,
+		constexpr ComponentMetadata(ComponentID id,
 		                  sizet size,
 		                  sizet alignment,
-		                  bool isTriviallyRelocatable,
-		                  DestructorFn destructorFn,
-						  void* destructorUserData,
-		                  MoveAndDestroyFn moveAndDestroyFn);
+		                  DestructionPolicyFn policyFn,
+		                  MoveAndDestroyFn moveAndDestroyFn)
+			: id(id), size(size), alignment(alignment),
+			  destructionPolicy(policyFn),
+			  moveAndDestroy(moveAndDestroyFn)
+		{}
 	};
 }
